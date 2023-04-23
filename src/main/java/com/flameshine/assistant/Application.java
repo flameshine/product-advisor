851d52e7f58672a601afefc6e44973d5ed46423e
Reichemann <excellenceissoon@gmail.com>
2023-04-23 20:47:13 +0300

    Update TODO

--- src/main/java/com/flameshine/assistant/Application.java
+++ src/main/java/com/flameshine/assistant/Application.java
@@ -3,7 +3,7 @@ package com.flameshine.assistant;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-// TODO: investigate recognition mismatches
+// TODO: investigate recognition mismatches (look at timeslices and audio recording settings)
 // TODO: add recording visualization
 
 @SpringBootApplication
