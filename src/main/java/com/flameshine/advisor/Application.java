1b1a3789920c9a89cf19766e08c60dae91f30586
Reichemann <excellenceissoon@gmail.com>
2023-05-10 15:02:30 +0300

    Encrypt secrets with git-crypt

--- src/main/java/com/flameshine/advisor/Application.java
+++ src/main/java/com/flameshine/advisor/Application.java
@@ -3,6 +3,8 @@ package com.flameshine.advisor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
+// TODO: document everything
+
 @SpringBootApplication
 public class Application {
 
