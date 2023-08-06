99915e834c3fb4dc64cfbeae416d774d1d32e3e4
Reichemann <excellenceissoon@gmail.com>
2023-08-06 19:35:21 +0300

    Update TODO list

--- src/main/java/com/flameshine/advisor/Application.java
+++ src/main/java/com/flameshine/advisor/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 // TODO: consider delegating speech recognition to AWS Transcribe
 // TODO: consider implementing the functionality of interacting using voice through the entire application flow
 // TODO: consider adding traditional methods of interacting with the web-site
+// TODO: unit tests
 
 /**
  * Entry point of the application.
